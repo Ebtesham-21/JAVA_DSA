03eb4636e509b59254632a14f3b63c21ba39ec6b
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-10 20:14:16 +0600

    sum of numbers

---
+++ SumofNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SumofNumbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int val = 1;
+        int sum = 0;
+
+        while (val <= n){
+            sum += val;
+            val++;
+        }
+
+        System.out.println("Sum of numbers from 1 to" + " " + n +" is " + sum);
+    }
+    
+}
