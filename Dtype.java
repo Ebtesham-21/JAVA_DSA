fb5d6392e1134a8fd4c736dcc98a5e8aa243913f
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-11 05:16:27 +0600

    dtype

---
+++ Dtype.java
@@ -0,0 +1,10 @@
+public class Dtype {
+    public static void main(String args[]) {
+        char ch = 'a';
+        byte b = 87;
+        System.out.println(ch);
+        System.out.println(b);
+
+    }
+    
+}
