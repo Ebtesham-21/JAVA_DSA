e77ff2054d8ecdc530b2a0c954328799f33ca764
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-11 04:54:11 +0600

    variable declare

---
+++ VariableDeclare.java
@@ -0,0 +1,14 @@
+public class VariableDeclare {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+
+        String name = "Android";
+        // a = b;
+
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
