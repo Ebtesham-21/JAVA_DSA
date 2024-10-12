4d126cdfe7314f7f6b135003c4617e86e2f3e160
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-13 01:02:42 +0600

    type casting

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String args[]){
+        float a = 3.33f;
+        int b = (int) a;
+        System.out.println(b);
+       
+        
+
+    }
+    
+}
