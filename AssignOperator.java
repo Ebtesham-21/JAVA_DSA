5f419298fc70bf01c2dc2e23a832b0a48c0a8804
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-14 04:24:39 +0600

    assignoperator

---
+++ AssignOperator.java
@@ -0,0 +1,11 @@
+public class AssignOperator {
+    public static void main(String args[]){
+        // int B = 5;
+        // B+= 10;
+        // System.out.println(B);
+        int B = 5;
+        B*= 10;
+        System.out.println(B);
+        // evabye jog biyog gun vag sob kora jay
+    }
+}
