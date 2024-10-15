7f9930024c39713aa6d3dc48d9e3d2c50f0760a3
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-15 23:07:27 +0600

    while loop

--- LeapYear.java
+++ LeapYear.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class LeapYear {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        // psudocode e vul korsi
         System.out.println("Enter Year");
         int year = sc.nextInt();
         if(year % 4 == 0 && year % 100 != 0 && year % 400 == 0){
---
+++ Print1To10.java
@@ -0,0 +1,10 @@
+public class Print1To10 {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num < 11){
+            System.err.println(num);
+            num++;
+        }
+    }
+    
+}
