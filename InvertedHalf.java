279b73bd51cd6065fc62c48cc08f2e6ad21145ad
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-18 03:21:46 +0600

    9 porjonto

--- InvertedHalf.java
+++ InvertedHalf.java
@@ -1,12 +1,10 @@
-public class NestedFor {
+public class InvertedHalf {
     public static void main(String args[]){
         for(int line = 4; line>=1; line--){
-            for(int star = 1; star <= line; star++){
-                System.out.print("*");
-            
+            for(int num = 4; num >= line; num--){
+                System.out.print(num);
             }
             System.out.println();
         }
     }
 }
- 
