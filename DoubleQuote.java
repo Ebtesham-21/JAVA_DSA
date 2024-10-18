b5a22bad6768df3d6bb071bfedac4b673b79358e
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-19 05:16:09 +0600

    space prb

---
+++ DoubleQuote.java
@@ -0,0 +1,5 @@
+public class DoubleQuote {
+    public static void main(String[] args) {
+        System.out.println("She said: \"Hello\" and then left.");
+    }
+}
---
+++ SpacePrb.java
@@ -0,0 +1,6 @@
+public class SpacePrb {
+    public static void main(String args[]){
+        String abc = "Lola";
+        System.out.println("Hello " + abc );
+    }
+}
