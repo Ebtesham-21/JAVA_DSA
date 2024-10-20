64a4080ae2c90d4395e492c71b3437d9f6385e21
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-20 23:48:30 +0600

    function param

--- FunctionSyntax.java
+++ FunctionSyntax.java
@@ -1,6 +1,7 @@
 public class FunctionSyntax {
 public static void printHello(){
     System.out.println("Hello");
+    return;
 }
     public static void main(String[] args) {
         printHello();
