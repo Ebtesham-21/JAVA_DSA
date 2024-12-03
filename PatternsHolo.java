cca9047eb5df9983eccee154d8a87ae5d56fadd6
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-03 08:37:32 +0600

    butterfly

--- PatternsHolo.java
+++ PatternsHolo.java
@@ -18,8 +18,115 @@ public class PatternsHolo {
 
          }
     }
+
+
+    public static void inverted_rotated_half_pyramid(int n){
+        // outer
+        for(int i=1; i<=n; i++){
+            // spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int s=1; s<=i; s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Inverted_half_pyramid(int n){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i + 1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floyds_triangle(int n){
+        int counter = 1;
+        for (int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+
+        System.out.println();
+        }
+        
+
+    }
+
+    public static void EVODDTRIANGLE(int n){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                if((j+i)%2 == 0){
+                    System.out.print(1);
+                }
+                else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // public static void Butterfly(int n){
+    //     for(int i = 1; i<=n; i++){
+    //         if(i<=n/2){
+    //             for(int j=1; j<=i; j++){
+    //                 System.out.print("*");
+    //                 System.out.print(" ");
+    //                 System.out.print("*");
+    //             }
+    //         } else {
+    //             for(int j = n/2; j>=1; j--){
+    //                 System.out.print("*");
+    //                 System.out.print(" ");
+    //                 System.out.print("*");
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    public static void Butterfly(int n) {
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
+
     public static void main(String[] args) {
-        hollow_rectangle(10, 7);
+        // hollow_rectangle(10, 7);
+        // inverted_rotated_half_pyramid(5);
+        // Inverted_half_pyramid(5);
+        // EVODDTRIANGLE(5);
+        Butterfly(5);
     }
     
 }
