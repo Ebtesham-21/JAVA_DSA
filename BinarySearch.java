53e6c6a8be1f2c19ceee1e87a66bfb1e2b1727e3
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-10 08:36:43 +0600

    java issue

---
+++ BinarySearch.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class BinarySearch {
+       public static void binalgo(String[] args){
+            int [] arr = {1, 2, 3, 4, 5, 6};
+            int guess = 4;
+            int low = 0;
+            int high = arr.length - 1;
+            while(low <= high){
+
+            };
+
+       };
+
+    }
+    
+
