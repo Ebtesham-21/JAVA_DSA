e910936a2d3a447e086f5b57e34b03d4d1dcac96
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-29 08:42:11 +0600

    time complexity

--- ArrayInOut.java
+++ ArrayInOut.java
@@ -1,7 +1,19 @@
 import java.util.*;
 public class ArrayInOut {
+
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i<numbers.length; i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+        }
+        return  largest;
+    }
     public static void main(String args[]){
         int marks[] = new int[100];
+        int numbers[] = {1, 2, 3, 5, 8};
+        System.out.println("largest is : " + getLargest(numbers));
         Scanner sc = new Scanner(System.in);
 
         marks[0] = sc.nextInt();
