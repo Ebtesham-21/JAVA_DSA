e9d0872fb92b162863dfa96a36fff39abaf94812
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-01 08:57:21 +0600

    array reverse swap method

--- Algorithoms.java
+++ Algorithoms.java
@@ -1,4 +1,5 @@
-public class BinarySearch {
+import java.util.*;
+public class Algorithoms {
 
      public static int binarySearch(int numbers[], int key){
           int start = 0; int end= numbers.length-1;
@@ -17,11 +18,27 @@ public class BinarySearch {
      return -1;
      }
 
+     // swap array reverse
+
+     public static void reverse(int numbers[]){
+          int start = 0; int end = numbers.length-1;
+          while(start < end){
+               int temp = numbers[end];
+               numbers[end] = numbers[start];
+               numbers[start] = temp;
+               start++;
+               end--;
+
+          }
+     }
+
      public static void main(String args[]){
           int numbers[] = {1, 2, 4, 5, 6, 8, 9};
           int key = 9;
 
-          System.out.println("Element found at index: " + binarySearch(numbers, key));
+          // System.out.println("Element found at index: " + binarySearch(numbers, key));
+          reverse(numbers);
+          System.out.println(Arrays.toString(numbers));
 
 
 
