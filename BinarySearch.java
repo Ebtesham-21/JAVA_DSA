085512d362839d624c454872ac5956fb37c5fdae
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-01 08:15:45 +0600

    binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,18 +1,33 @@
-
-
-
-
 public class BinarySearch {
-       public static void binalgo(String[] args){
-            int [] arr = {1, 2, 3, 4, 5, 6};
-            int guess = 4;
-            int low = 0;
-            int high = arr.length - 1;
-            while(low <= high){
 
-            };
+     public static int binarySearch(int numbers[], int key){
+          int start = 0; int end= numbers.length-1;
+          while(start <= end){
+               int mid = (start + end)/2;
 
-       };
+               if(numbers[mid] == key){
+                    return mid;
+               }
+               else if(numbers[mid] < key){
+                    start = mid + 1;
+               } else {
+                    end = mid - 1;
+               }
+          }
+     return -1;
+     }
+
+     public static void main(String args[]){
+          int numbers[] = {1, 2, 4, 5, 6, 8, 9};
+          int key = 9;
+
+          System.out.println("Element found at index: " + binarySearch(numbers, key));
+
+
+
+     }
+
+     
 
     }
     
