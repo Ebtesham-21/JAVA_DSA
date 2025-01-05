2e7f2059a54982218a52303b3a1932e260dc7eaa
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-05 08:23:15 +0600

    pairs array

--- Algorithoms.java
+++ Algorithoms.java
@@ -32,6 +32,16 @@ public class Algorithoms {
           }
      }
 
+     public static void printPairs(int numbers[]){
+          for(int i=0; i<numbers.length; i++){
+               int curr = numbers[i];
+               for(int j=i+1; j<numbers.length; j++){
+                    System.out.print("(" + curr + "," +numbers[j]+ ")");
+               }
+               System.out.println();
+          }
+     }
+
      public static void main(String args[]){
           int numbers[] = {1, 2, 4, 5, 6, 8, 9};
           int key = 9;
@@ -42,7 +52,7 @@ public class Algorithoms {
 
 
 
-     }
+     } 
 
      
 
